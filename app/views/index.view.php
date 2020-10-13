9281ecc2a62000930d51bc5b47ad5a89479f097b
Justin MacAlpine <justin.macalpine@gmail.com>
2020-10-13 09:51:59 -0700

    Initial Commit (v1.0)

---
+++ app/views/index.view.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php'); ?>
+
+    <h1>Home Page</h1>
+
+<h1>Form</h1>
+
+<form method="POST" action="/users">
+    <input name="name"></input>
+    <button type="submit">Submit</button>
+</form>
+
+<?php require('partials/footer.php'); ?>
